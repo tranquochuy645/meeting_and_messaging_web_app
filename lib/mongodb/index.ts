98b3278a1a1d67cf64d05c78a4543932311d4b5a
huy <tranquochuy645@gmail.com>
2023-06-21 19:58:57 +0700

    acp

--- lib/mongodb/index.ts
+++ lib/mongodb/index.ts
@@ -9,6 +9,8 @@ if (!mongo_uri || !db_name) {
 }
 
 let db: Db | null | undefined;
+// this should be called only once befor starting the application
+// or when connection is failed
 
 export const connectToDb = (callback?: (err?: Error) => void): void => {
   MongoClient.connect(mongo_uri, opts)
@@ -26,6 +28,8 @@ export const connectToDb = (callback?: (err?: Error) => void): void => {
     });
 };
 
+
+// this should be called to get the reference to db object where ever needed
 export const getDb = (): Db | null | undefined => {
   return db;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
 import styles from './page.module.css'
 import { getDb, connectToDb } from '../../lib/mongodb'
+// import {login,register} from '../../lib/auth'
 let db;
 connectToDb(
   err => {
     if (err) {
       console.log(err);
     } else {
-      db = getDb();
       console.log("Connected to database");
     }
   }
 )
+const callLogin = async (data: any) => {
+  "use server"
+  console.log(data)
+}
 export default function Home() {
   return (
     <main>
-      HI MOM
+      <form action={callLogin} className='form'>
+        <div className='form-row'>
+          <label htmlFor='username'>
+            Username
+          </label>
+          <input type='text' name='username' id='username' />
+        </div>
+        <div className='form-row'>
+          <label htmlFor='password'>
+            Password
+          </label>
+          <input type='text' name='password' id='password' />
+        </div>
+        <div className='form-row'>
+          <button className='btn'>Login</button>
+        </div>
+      </form>
     </main>
+
   )
 }
