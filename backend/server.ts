7ced3d95d188196f4e71c2050d23cada473d1c6e
huy <tranquochuy645@gmail.com>
2023-06-23 17:00:14 +0700

    acp

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,11 @@
-console.log("HI MOM")
+import {config} from 'dotenv'
+import express from 'express'
+import {createServer} from 'http'
+config();
+
+const port = process.env.PORT;
+const app=express();
+app.use(express.static('public'));
+const server = createServer(app);
+server.listen(port);
+
