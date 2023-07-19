5f8d2fbde215d98ba188dc11c49b5e525d8fc9a3
huy <tranquochuy645@gmail.com>
2023-07-19 14:58:02 +0700

    acp

--- release/src/controllers/mongodb/CRUD/CollectionOriented/generic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CollectionReference = void 0;
-class CollectionReference {
-    constructor(ref) {
-        this._collection = ref;
-    }
-}
-exports.CollectionReference = CollectionReference;
--- release/src/controllers/mongodb/CRUD/CollectionOriented/meetings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generic_1 = require("./generic");
-class MeetingsController extends generic_1.CollectionReference {
-}
-exports.default = MeetingsController;
