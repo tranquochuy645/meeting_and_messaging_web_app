51b8937813b94fb522fed1113422e6c1e25faba0
tranquochuy645 <tranquochuy645@gmail.com>
2024-01-16 19:23:53 +0700

    test

--- backend/build.sh
+++ backend/build.sh
@@ -6,9 +6,9 @@ rm -rf dist
 tsc &&
  
 # Copy package.json to dist directory
-echo "Copying package.json to dist..."
+echo "Copying package.json and Dockerfile to dist..."
 cp package.json dist/ &&
-
+cp DockerTemplate/* dist/ &&
 
 # Move contents of dist to release directory
 echo "moving to release..." 
