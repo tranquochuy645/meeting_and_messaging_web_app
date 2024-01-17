0f48f7fbadf4e9e7b8203c4cd8bce089f48dd3fa
tranquochuy645 <tranquochuy645@gmail.com>
2024-01-18 04:28:05 +0700

    fix typo

--- ci/template/start.sh
+++ ci/template/start.sh
@@ -2,9 +2,8 @@
 
 image_name="chat_app"
 
-# Generate a timestamp for the log file
-timestamp=$(date +"%Y_%m_%d_%H_%M_%S")
-build_log="../logs/${image_name}_build_log_$timestamp.txt"
+build_log="../logs/${image_name}_build_log_$(date +"%Y_%m_%d_%H_%M_%S").txt"
+
 mkdir "../logs" &>/dev/null
 touch "../logs/${image_name}_build_log_$timestamp.txt"
 
@@ -19,22 +18,20 @@ done
 # Check if .env file exists
 if [ ! -e ".env" ]; then
     echo "Error: .env file is missing." >&2
-    echo "Error: .env file is missing." >>$build_log
     exit 1
 fi
 
-echo "Stopping old containers matching image name: $image_name" $ >>"$build_log"
-docker stop $(docker ps -q -f ancestor=$image_name --format "{{.ID}}") $ >>"$build_log"
+echo "Stopping old containers matching image name: $image_name"
+docker stop $(docker ps -q -f ancestor=$image_name --format "{{.ID}}") &>>"$build_log"
 
 echo "Removing old containers matching image name: $image_name"
-docker rm $(docker ps -a -q -f ancestor=$image_name --format "{{.ID}}") $ >>"$build_log"
+docker rm $(docker ps -a -q -f ancestor=$image_name --format "{{.ID}}") &>>"$build_log"
 
-# Remove image
-echo "Removing old image: $image_name" $ >>"$build_log"
-docker rmi $image_name $ >>"$build_log"
+echo "Removing old image: $image_name"
+docker rmi $image_name &>>"$build_log"
 
 # Rebuild the Docker image
-echo "Building Docker image..." $ >>"$build_log"
+echo "Building Docker image..."
 if docker build -t $image_name . &>>"$build_log"; then
     echo "Docker image build successful."
 else
@@ -49,7 +46,8 @@ docker run -d --restart unless-stopped \
     --env-file .env \
     -p 8080:8080 \
     $image_name &&
-    echo "Process completed successfully" && exit
+    echo "Process completed successfully" && 
+    exit
 
-echo "Error: Docker run failed check log at $build_log" >$2
+echo "Error: Docker run failed. Check log at $build_log" >$2
 exit 1
