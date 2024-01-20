1c4fb0b31f7bd8d6a1184caa44b6c0d8dc7ad814
baotq02 <baotq02@gmail.com>
2024-01-20 23:59:16 +0700

    precall btn adjust

--- frontend/src/components/AskForMediaDevices/index.tsx
+++ frontend/src/components/AskForMediaDevices/index.tsx
@@ -1,22 +1,27 @@
-import { FC } from "react"
-import './style.css'
+import { FC } from "react";
+import "./style.css";
 interface AskForMediaDevicesProps {
-    onReady: (stream: MediaStream) => void;
+  onReady: (stream: MediaStream) => void;
 }
 const AskForMediaDevices: FC<AskForMediaDevicesProps> = ({ onReady }) => {
-    const initialize = async () => {
-        const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        if (localStream) {
-            onReady(localStream);
-        } else {
-            throw new Error("Can not get user media")
-        }
+  const initialize = async () => {
+    const localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    if (localStream) {
+      onReady(localStream);
+    } else {
+      throw new Error("Can not get user media");
     }
-    return (
-        <>
-            <p> You need to allow access to camera and mic</p>
-            <button onClick={() => initialize()}>Allow</button>
-        </>
-    )
-}
-export default AskForMediaDevices
+  };
+  return (
+    <>
+      <p> You need to allow access to camera and mic</p>
+      <button id="precall_btn" onClick={() => initialize()}>
+        Allow
+      </button>
+    </>
+  );
+};
+export default AskForMediaDevices;
